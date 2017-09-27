fee23e3e11bdc07a181c5bce5f5cced325c92b3a
Da-Tony-W <dw6@ualberta.ca>
2017-09-27 14:10:46 -0600

    initial

---
+++ app/src/main/java/com/example/android/dw6_countbook/CBOpenHelper.java
@@ -0,0 +1,27 @@
+package com.example.android.dw6_countbook;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Tony on 2017/9/26.
+ */
+
+public class CBOpenHelper extends SQLiteOpenHelper{
+
+
+    public CBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
