ed847074610709acd12085c3870f9ef6853cbdf5
Da-Tony-W <dw6@ualberta.ca>
2017-09-30 15:10:10 -0600

    gui change

--- app/src/main/java/com/example/android/dw6_countbook/MainActivity.java
+++ app/src/main/java/com/example/android/dw6_countbook/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.android.dw6_countbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FILENAME = "file.sav";
+    private ListView counterListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        Button buttonDelete = (Button) findViewById(R.id.buttonDelete);
+
+        counterListView =(ListView) findViewById(R.id.counterListView);
+
+
+
+
+
+
+
+
     }
 }
